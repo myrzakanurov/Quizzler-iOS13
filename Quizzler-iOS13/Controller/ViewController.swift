e89bc4a07d9036d512757a6785e724dec6865d50
myrzakanurov <myrzakanurov.a@gmail.com>
2021-02-22 01:33:19 +0600

    Completed Version

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var answerButtonOne: UIButton!
+    @IBOutlet weak var answerButtonTwo: UIButton!
+    @IBOutlet weak var answerButtonThree: UIButton!
+    
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle! // True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target:self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        questionLabel.text = quizBrain.getQuestionText()
+        answerButtonOne.setTitle(quizBrain.getAnswer1(), for: .normal)
+        answerButtonTwo.setTitle(quizBrain.getAnswer2(), for: .normal)
+        answerButtonThree.setTitle(quizBrain.getAnswer3(), for: .normal)
+        
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        answerButtonOne.backgroundColor = UIColor.clear
+        answerButtonTwo.backgroundColor = UIColor.clear
+        answerButtonThree.backgroundColor = UIColor.clear
+    }
+    
+}
+
---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Arman Myrzakanurov on 21.02.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        self.text = q
+        self.answer = a
+        self.correctAnswer = correctAnswer
+    }
+}
